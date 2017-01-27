c855f62a3c8a41fc6fe83851a3647f8ed2880c46
Anar Enhsaihan <aenhsaihan@gmail.com>
2017-01-27 07:16:22 +0000

    Need to open calculator.rb on local sublime editor because Cloud9 does not like MSG

--- 101 Programming Foundations/lesson_2/calculator.rb
+++ 101 Programming Foundations/lesson_2/calculator.rb
@@ -1,30 +1,92 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
+def prompt(message)
+	Kernel.puts("=> #{message}")	
+end
 
-Kernel.puts("Welcome to the Calculator")
+def valid_number?(num)
+	num.to_i() != 0
+end
 
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp
+prompt("Welcome to the Calculator! Enter your name:")
 
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp
+name = ''
+loop do
+	name = Kernel.gets.chomp
+	
+	if name.empty?
+		prompt("Make sure to use a valid name")
+	else
+		break
+	end
+end
 
-Kernel.puts("What operation would you like to perform on these two numbers?\n 1) add\n 2) subtract\n 3) multiply\n 4) divide")
-operator = Kernel.gets.chomp
+prompt("Hi #{name}")
 
- 
-result =	if operator == '1'
-						number1.to_i + number2.to_i
-					elsif operator == '2'
-						number1.to_i - number2.to_i
-					elsif operator == '3'
-						number1.to_i * number2.to_i
-					elsif operator == '4'
-						number1.to_f / number2.to_f
-					else
-						"Unrecognized command"
-					end
+loop do #main loop
+	
+	number1 = ''
+	loop do
+		prompt("What's the first number?")
+		number1 = Kernel.gets().chomp
+		
+		if valid_number?(number1)
+			break
+		else
+			prompt("Hmm, that doesn't appear to be a valid number")
+		end
+	end
+	
+	number2 = ''
+	loop do
+		prompt("What's the second number?")
+		number2 = Kernel.gets().chomp
+		
+		if valid_number?(number2)
+			break
+		else
+			prompt("Hmm, that doesn't appear to be a valid number")
+		end
+	end
+	
+	operator_prompt = <<-MSG
+		What operation would you like to perform on these two numbers?
+		1) add
+		2) subtract
+		3) multiply
+		4) divide
+	MSG
+	
+	# prompt("What operation would you like to perform on these two numbers?\n 1) add\n 2) subtract\n 3) multiply\n 4) divide")
+	
+	prompt(operator_prompt)
+	
+	operator = ''
+	loop do
+		operator = Kernel.gets.chomp
+		
+		if %w(1 2 3 4).include?(operator)
+			break
+		else
+			prompt("Must choose 1, 2, 3 or 4")
+		end
+	end
+	
+	result = case operator
+						when '1'
+							number1.to_i + number2.to_i
+					 	when '2'
+					 		number1.to_i - number2.to_i
+					 	when '3'
+					 		number1.to_i * number2.to_i
+					 	when '4'
+					 		number1.to_f / number2.to_f
+	end
+	
+	prompt("The result is #{result}")
+	
+	prompt("Do you want to perform another calculation? (Y to calculate again)")
+	answer = Kernel.gets().chomp
+	
+	break unless answer.downcase().start_with?('y')
+end
 
-Kernel.puts("The result is #{result}")
+prompt("Thank you for using the calculator!")
