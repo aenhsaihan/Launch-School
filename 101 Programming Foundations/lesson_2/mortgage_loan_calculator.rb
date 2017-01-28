38635d3abaa97d43cdf5c49283db7ec446a79972
saikhan <aenhsaihan@gmail.com>
2017-01-28 14:40:57 +0700

    Convert tabs to 2 spaces

--- 101 Programming Foundations/lesson_2/mortgage_loan_calculator.rb
+++ 101 Programming Foundations/lesson_2/mortgage_loan_calculator.rb
@@ -2,85 +2,85 @@ require 'yaml'
 MESSAGES = YAML.load_file("mortgage_configuration.yml")
 
 def prompt(key)
-	message = MESSAGES[key]
-	puts "=> #{message}"
+  message = MESSAGES[key]
+  puts "=> #{message}"
 end
 
 def float?(num)
-	Float(num) rescue false
+  Float(num) rescue false
 end
 
 def integer?(num)
-	Integer(num) rescue false
+  Integer(num) rescue false
 end
 
 def number?(num)
-	integer?(num) || float?(num)
+  integer?(num) || float?(num)
 end
 
 def valid_number?(num)
-	number?(num)
+  number?(num)
 end
 
 def calculate_monthly_interest_rate(apr)
-	apr / 12
+  apr / 12
 end
 
 def calculate_monthly_payment(loan_amount, apr, loan_duration_months)
-	p = loan_amount.to_f
-	n = loan_duration_months.to_i
-	j = calculate_monthly_interest_rate(apr)
+  p = loan_amount.to_f
+  n = loan_duration_months.to_i
+  j = calculate_monthly_interest_rate(apr)
 
-	m = p * (j / (1 - (1 + j)**(-n)))
+  m = p * (j / (1 - (1 + j)**(-n)))
 
-	'%.2f' % m
+  '%.2f' % m
 end
 
 loop do # main loop
-	
-	loan_amount = ''
-	loop do 
-		prompt("loan_amount")
-		loan_amount = gets.chomp
+  
+  loan_amount = ''
+  loop do 
+    prompt("loan_amount")
+    loan_amount = gets.chomp
 
-		if valid_number?(loan_amount)
-			break
-		else
-			prompt("number_error")
-		end
-	end
+    if valid_number?(loan_amount)
+      break
+    else
+      prompt("number_error")
+    end
+  end
 
-	apr = ''
-	loop do 
-		prompt("apr")
-		apr = (gets.chomp.to_f / 100)
+  apr = ''
+  loop do 
+    prompt("apr")
+    apr = (gets.chomp.to_f / 100)
 
-		if valid_number?(apr)
-			break
-		else
-			prompt("number_error")
-		end
-	end
+    if valid_number?(apr)
+      break
+    else
+      prompt("number_error")
+    end
+  end
 
-	loan_duration_months = ''
-	loop do 
-		prompt("loan_duration")
-		loan_duration_months = gets.chomp
+  loan_duration_months = ''
+  loop do 
+    prompt("loan_duration")
+    loan_duration_months = gets.chomp
 
-		if valid_number?(loan_duration_months)
-			break
-		else
-			prompt("number_error")
-		end
-	end
+    if valid_number?(loan_duration_months)
+      break
+    else
+      prompt("number_error")
+    end
+  end
 
-	monthly_payment = calculate_monthly_payment(loan_amount, apr, loan_duration_months)
-	puts "Your monthly payment is $#{monthly_payment}"
+  monthly_payment = calculate_monthly_payment(loan_amount, apr, loan_duration_months)
+  puts "Your monthly payment is $#{monthly_payment}"
 
-	prompt("subsequent_request")
-	answer = gets.chomp
-	
-	break unless answer.downcase().start_with?('y')
+  prompt("subsequent_request")
+  answer = gets.chomp
+  
+  break unless answer.downcase().start_with?('y')
 
 end
 
