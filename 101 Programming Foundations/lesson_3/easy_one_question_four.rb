c3298823f747f78abee3b93fd2df0bb1a5115e9a
saikhan <aenhsaihan@gmail.com>
2017-01-31 18:00:09 +0700

    Complete fourth quesiton of first easy set

---
+++ 101 Programming Foundations/lesson_3/easy_one_question_four.rb
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete_at(1)
+numbers.delete(1)
+
+# numbers.delete_at(1) would delete the element at the second index and return it
+# numbers.delete(1) would delete 1 from the collection and return it
