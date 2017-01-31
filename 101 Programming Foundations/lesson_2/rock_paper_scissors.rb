208c3227e7b6a13db2323000098db0e7ce3d41ae
saikhan <aenhsaihan@gmail.com>
2017-01-31 15:50:14 +0700

    Implement first and second requirements of bonus RPS

--- 101 Programming Foundations/lesson_2/rock_paper_scissors.rb
+++ 101 Programming Foundations/lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,33 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w[rock paper scissors lizard spock]
 
 def prompt(message)
 	puts "=> #{message}"
 end
 
+def valid_letter?(letter)
+	if letter == 's' then return false end
+	VALID_CHOICES.map { |choice| choice.chr }.include?(letter)
+end
+
+def valid_choice?(choice)
+	if choice.length > 1
+		VALID_CHOICES.include?(choice)
+	else
+		valid_letter?(choice)
+	end
+end
+
 def win?(player_one, player_two)
-	(player_one == 'rock' && player_two == 'scissors') ||
+	(player_one == 'scissors' && player_two == 'paper') ||
 	(player_one == 'paper' && player_two == 'rock') ||
-	(player_one == 'scissors' && player_two == 'paper')
+	(player_one == 'rock' && player_two == 'lizard') ||
+	(player_one == 'lizard' && player_two == 'spock') ||
+	(player_one == 'spock' && player_two == 'scissors') ||
+	(player_one == 'scissors' && player_two == 'lizard') ||
+	(player_one == 'lizard' && player_two == 'paper') ||
+	(player_one == 'paper' && player_two == 'spock') ||
+	(player_one == 'spock' && player_two == 'rock') ||
+	(player_one == 'rock' && player_two == 'scissors')
 end
 
 def display_results(player, computer)
@@ -20,24 +40,36 @@ def display_results(player, computer)
 	end
 end
 
+def rewrite_choice(user_choice)
+	if user_choice.length > 1
+		user_choice
+  else
+    VALID_CHOICES.select { |choice| choice.start_with?(user_choice) }.first
+  end
+end
+
 loop do
 	choice = ''
 	loop do
 		prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-		choice = gets.chomp
+		choice = gets.chomp.downcase
 
-		if VALID_CHOICES.include?(choice)
+		if valid_choice?(choice)
 			break
 		else
-			prompt "That's not a valid choice"
+			if choice == 's'
+				prompt "Please choose between scissors and spock"
+			else
+				prompt "That's not a valid choice"
+			end
 		end
 	end
 
 	computer_choice = VALID_CHOICES.sample
 
-	puts "You chose: #{choice}; Computer chose: #{computer_choice}"
+	puts "You chose: #{rewrite_choice(choice)}; Computer chose: #{computer_choice}"
 
-	display_results choice, computer_choice
+	display_results rewrite_choice(choice), computer_choice
 
 	prompt "Do you want to play again?"
 	answer = gets.chomp
