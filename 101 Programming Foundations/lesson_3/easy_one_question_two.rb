44d0981e8857b6980a3a8775a10d0abbd7b3ae40
saikhan <aenhsaihan@gmail.com>
2017-01-31 17:43:08 +0700

    Commit

---
+++ 101 Programming Foundations/lesson_3/easy_one_question_two.rb
@@ -0,0 +1,9 @@
+# ? is a convention used in ruby to indicate that the method will return a boolean value
+# ! is  a convention used in ruby to indicate that the method may cause undesired side-effects
+
+# != is used to say something is NOT EQUAL to something. It can be used anywhere where you check for inequality
+# !user_name would negate the value in user_name. So if user_name were true, it would negate it to false, and vice-versa.
+# words.uniq! would mutate the original collection by removig duplicate elements from the collection permanently
+# putting ? before something may cause an error if Ruby does not allow you to name variable as such
+# putting ? after something doesn't do anything, it is simply a convention
+# putting !! before something would do nothing ultimately, since !! would cancel itself out
