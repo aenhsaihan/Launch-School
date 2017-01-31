2f11840c83a28ba901bd2238fe28e324867e90d3
saikhan <aenhsaihan@gmail.com>
2017-01-31 18:57:19 +0700

    Complete exercise nine of the first easy set

---
+++ 101 Programming Foundations/lesson_3/easy_one_question_nine.rb
@@ -0,0 +1,6 @@
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+array = []
+
+array << flintstones.keys.select { |name| name == "Barney" }.first
+array << flintstones["Barney"]
