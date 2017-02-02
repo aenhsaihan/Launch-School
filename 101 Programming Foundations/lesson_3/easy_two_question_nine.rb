a417cfb4809c912f9d8cadefc3250e5222b2bd51
saikhan <aenhsaihan@gmail.com>
2017-02-02 16:07:56 +0700

    Complete question nine in second easy set

---
+++ 101 Programming Foundations/lesson_3/easy_two_question_nine.rb
@@ -0,0 +1,22 @@
+statement = "The Flintstones Rock!"
+
+# Understand the problem
+
+# Write a one line program to count the number of lower-case t characters in the string
+
+# => Input
+# statement = "The Flintstones Rock!"
+
+# => Output
+# 2
+
+# Examples/Test Cases
+
+# Data Structure
+# We are using a string
+
+# Algorithm
+#
+# Count how many lowercase t's there are in the string statement
+
+statement.count 't'
