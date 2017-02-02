8df100aa03fe89520d4d376bb257956c363591ff
saikhan <aenhsaihan@gmail.com>
2017-02-02 13:44:07 +0700

    Complete first question of the second easy set

--- 101 Programming Foundations/lesson_3/easy_two_question_one.rb
+++ 101 Programming Foundations/lesson_3/easy_two_question_one.rb
@@ -27,3 +27,11 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 # => Use a built-in hash method to discover whether Spot is a key or not
 
 puts ages.fetch('Spot') { |el| "#{el} is not present in the hash"}
+
+ages.key?("Spot")
+
+ages.has_key?("Spot")
+
+ages.include?("Spot")
+
+ages.member?("Spot")
