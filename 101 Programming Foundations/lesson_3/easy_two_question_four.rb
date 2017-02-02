31ab1a55990439c10aa48843e2d93fd344450ba9
saikhan <aenhsaihan@gmail.com>
2017-02-02 15:09:40 +0700

    Complete question four of the second easy set

---
+++ 101 Programming Foundations/lesson_3/easy_two_question_four.rb
@@ -0,0 +1,26 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+# Understand the problem
+
+# We need to see if the string Fido appears in advice
+
+# => Input
+# advice
+
+# => Output
+# true
+# false
+
+# Examples / Test Cases
+
+# advice.find?("Fido")
+
+# Data Structure
+#
+# We are working with a string
+
+# Algorithm
+#
+# Find the string Fido within the advice string
+
+advice.include? "Fido"
