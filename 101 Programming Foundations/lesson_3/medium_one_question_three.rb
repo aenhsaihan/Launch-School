d5c618de804a5e57fbd07cc96e899cb532ef21aa
saikhan <aenhsaihan@gmail.com>
2017-02-02 18:07:50 +0700

    Complete bonus questions

--- 101 Programming Foundations/lesson_3/medium_one_question_three.rb
+++ 101 Programming Foundations/lesson_3/medium_one_question_three.rb
@@ -7,3 +7,9 @@ def factors(number)
   end
   divisors
 end
+
+# What is the purpose of the number % dividend == 0 ?
+# The purpose is to see if the number is divisible by the dividend without producing a remainder
+
+# What is the purpose of the second-to-last line in the method (the divisors before the method's end)?
+# It returns an arary of all the factors for the number that was passed to the method.
