51ed14d8576879a0f995a08dc39f9f54546367de
saikhan <aenhsaihan@gmail.com>
2017-02-02 15:28:04 +0700

    Complete question six of second easy set

---
+++ 101 Programming Foundations/lesson_3/easy_two_question_six.rb
@@ -0,0 +1,27 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# Understand the problem
+
+# We need to add Fido into the array above
+
+# => Input
+#
+# %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# => Output
+#
+# %w(Fred Barney Wilma Betty BamBam Pebbles Fido)
+
+# Examples / Test Cases
+#
+# flintstones.push('Fido')
+
+# Data Structure
+#
+# Array is the data structure
+
+# Algorithm
+#
+# Push Fido into the flintstones array
+
+flintstones.push 'Fido'
