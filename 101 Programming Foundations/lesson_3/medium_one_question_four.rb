9e2ef8af882aaf9ed3709c015346d108dab283aa
saikhan <aenhsaihan@gmail.com>
2017-02-02 18:23:05 +0700

    Complete question four of the first medium set

---
+++ 101 Programming Foundations/lesson_3/medium_one_question_four.rb
@@ -0,0 +1,14 @@
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+# The method with the << operator will permanently mutate the buffer outside of the method
+# Therefore, that method is returning a value and it also has a side effect, which is a red flag
