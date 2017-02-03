a137797166235b440baad4878a72f799a1d32018
saikhan <aenhsaihan@gmail.com>
2017-02-03 18:52:54 +0700

    Update question three to include B and C

--- 101 Programming Foundations/lesson_3/hard_one_question_three.rb
+++ 101 Programming Foundations/lesson_3/hard_one_question_three.rb
@@ -1,3 +1,5 @@
+# A:
+
 def mess_with_vars(one, two, three)
   one = two
   two = three
@@ -23,3 +25,39 @@ mess_with_vars(one, two, three)
 puts "one is: #{one}" # => "one is: one"
 puts "two is: #{two}" # => "one is: two"
 puts "three is: #{three}" # => "one is: three"
+
+# B:
+
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # => "one is: one"
+puts "two is: #{two}" # => "one is: two"
+puts "three is: #{three}" # => "one is: three"
+
+# C:
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # => "one is: two"
+puts "two is: #{two}" # => "two is: three"
+puts "three is: #{three}" # => "three is: one"
