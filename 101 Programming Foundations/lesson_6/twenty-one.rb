788c3d9e7ac7cd9939d568e63f34edbe29d2c565
saikhan <aenhsaihan@gmail.com>
2017-02-24 16:44:12 +0700

    Allow user to hit until he busts

--- 101 Programming Foundations/lesson_6/twenty-one.rb
+++ 101 Programming Foundations/lesson_6/twenty-one.rb
@@ -31,17 +31,51 @@ def total(cards)
   sum
 end
 
-def busted?
-  false
+def busted?(cards)
+  if total(cards) > 21
+    true
+  else
+    false
+  end
 end
+
+def decide_winner
+  # determine who won between the player and the dealer
+end
+
+def display_winner
+  puts "The winner is #{decide_winner}"
+end
+
+def deal_cards(player_cards, dealer_cards, deck)
+  2.times { player_cards << deck.delete(deck.sample)}
+  2.times { dealer_cards << deck.delete(deck.sample)}
+end
+
+def dealer_decision(dealer_cards)
+  total(dealer_cards) < 17 ? 'hit' : 'stay'
+end
+
+def inform_player(player_cards, dealer_cards)
+  known_card = dealer_cards.first
+  puts "Dealer has #{known_card} and an unknown card"
+
+  puts "You have #{player_cards}"
+end
+
+player_cards = []
+dealer_cards = []
+deal_cards(player_cards, dealer_cards, deck)
 
 loop do
+  inform_player(player_cards, dealer_cards)
   puts 'hit or stay?'
   answer = gets.chomp.downcase
-  break if answer == 'stay' || busted?
+  break if answer == 'stay' || busted?(player_cards)
+  player_cards << deck.delete(deck.sample)
 end
 
-if busted?
+if busted?(player_cards)
   puts 'You busted!'
 else
   puts 'You stayed!'
@@ -50,14 +84,14 @@ end
 # onto the dealer
 answer = ''
 loop do
-  break if answer == 'stay' || busted?
-  # answer = dealer_decision method invoked here
+  break if answer == 'stay' || busted?(dealer_cards)
+  answer = dealer_decision(dealer_cards)
 end
 
-if busted?
+if busted?(dealer_cards)
   puts 'Dealer busted!'
 else
   puts 'Dealer stayed!'
 end
 
-
+display_winner
